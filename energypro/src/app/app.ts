e8b1a5e3f4ae0a4b96bbc4995be478610162d415
goncalo.fonte <goncalo.fonte@citin.pt>
2025-09-21 23:32:37 +0100

    feature: body done

--- energypro/src/app/app.ts
+++ energypro/src/app/app.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { Navbar } from './navbar/navbar';
 import { Home } from './home/home';
 import { About } from './about/about';
+import { Services } from './services/services';
+import { Contacts } from './contacts/contacts';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Home, About],
+  imports: [Navbar, Home, About, Services, Contacts, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
