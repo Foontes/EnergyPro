2e796df20199250d108f0294596de5280b34060b
goncalo.fonte <goncalo.fonte@citin.pt>
2025-09-21 21:01:01 +0100

    feature: test commit

--- energypro/src/app/app.ts
+++ energypro/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from './navbar/navbar';
 import { Home } from './home/home';
+import { About } from './about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Home],
+  imports: [Navbar, Home, About],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
